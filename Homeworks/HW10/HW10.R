f22a6b38dac3e7307070e36a75138e157f882d18
dnackat <dileepbn@gmail.com>
2018-11-26 16:33:27 +0530

    working on hw10...

--- Homeworks/HW10/HW10.R
+++ Homeworks/HW10/HW10.R
@@ -12,9 +12,23 @@ data_hw10 <- as.data.frame(read.csv("census80.csv"))
 summary(data_hw10)
 
 # Instrumental variable for whether there was multiple pregnancy during the 2nd pregnancy
-data_mult2nd <- filter(data_hw10, ageq2nd == ageq3rd)
+data_hw10$mult2nd <- as.numeric(data_hw10$ageq2nd == data_hw10$ageq3rd)
 
 # Proportion of such households
-nrow(data_mult2nd)/nrow(data_hw10)
+sum(data_hw10$mult2nd, na.rm = TRUE)/nrow(data_hw10)
 
 # Instrumental variable for whether the first and second child have the same sex
+data_hw10$sex1st2nd <- as.numeric(data_hw10$sex1st == data_hw10$sex2nd)
+
+# Proportion of such households
+sum(data_hw10$sex1st2nd, na.rm = TRUE)/nrow(data_hw10)
+
+# Run an OLS model for labor supply: 
+ols1 <- lm(formula = workedm ~ I(numberkids - 1) + blackm + hispm + othracem, data = data_hw10)
+summary(ols1)
+
+ols2 <- lm(formula = weeksm ~ I(numberkids - 1) + blackm + hispm + othracem, data = data_hw10)
+summary(ols2)
+
+# 
+
