6e70d5e5348b960b26dbbe994c0b0b1574124429
dnackat <dileepbn@gmail.com>
2018-12-05 03:41:34 +0530

    working on the final assessment.....

--- Final_Exam/final_q6_analysis.R
+++ Final_Exam/final_q6_analysis.R
@@ -32,4 +32,21 @@ ggplot(data = df_q6, aes(total_budget)) +
   geom_density(data = df_c, kernel = "gaussian", aes(total_budget), color = "darkblue")
 
 # Calculate the average treatment effect (ATE) for pct_missing
-ate_pct_missing <- mean(df_t$pct_missing) - mean(df_c$pct_missing)
+t_avg <- mean(df_t$pct_missing)
+c_avg <- mean(df_c$pct_missing)
+ate_pct_missing <- t_avg - c_avg
+
+# Neyman standard error
+n_t <- nrow(df_t)
+n_c <- nrow(df_c)
+s_t_2 <- (1/(n_t-1))*sum((df_t$pct_missing - t_avg)^2)
+s_c_2 <- (1/(n_c-1))*sum((df_c$pct_missing - c_avg)^2)
+v_neyman <- sqrt((s_t_2/n_t) + (s_c_2/n_c))
+
+# Test statistic for hypothesis testing
+t_stat <- ate_pct_missing/v_neyman
+
+# The associated p-value
+p_val <- 2*(1-pnorm(0.7538))
+
+# p_val > 0.05 (95% CI), so we fail to reject the null
